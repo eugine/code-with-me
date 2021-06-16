9acad0f987aad6dcfd5032fb1af83dc02d1199d9
Evgeniy Sokolov <evgeniy.sokolov@matmatch.com>
2021-06-16 14:36:55 +0300

    Initial project

---
+++ src/main/java/com/solarisbank/demo/codewithme/CodeWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.solarisbank.demo.codewithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeWithMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeWithMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solarisbank/demo/codewithme/CodeWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solarisbank.demo.codewithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
