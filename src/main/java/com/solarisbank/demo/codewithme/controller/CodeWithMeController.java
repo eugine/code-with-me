e59539409015da236f0464d69a2cddf45dc5cf7e
Evgeniy Sokolov <evgeniy.sokolov@matmatch.com>
2021-06-16 15:01:03 +0300

    Added a dependency

--- src/main/java/com/solarisbank/demo/codewithme/controller/CodeWithMeController.java
+++ src/main/java/com/solarisbank/demo/codewithme/controller/CodeWithMeController.java
@@ -4,12 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.apache.commons.text.StringEscapeUtils.escapeXml10;
+
 @RestController
 @RequestMapping("/api/code-with-me")
 public class CodeWithMeController {
 
     @GetMapping
     public String hello() {
-        return "Code With Me";
+        return escapeXml10("Code With Me");
     }
 }
